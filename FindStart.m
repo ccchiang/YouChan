07cab9b3c1d6c1a10d8afae495346932256a18f5
ccchiang <ccchiang.tw@gmail.com>
2013-07-04 18:36:57 +0800

    Remove the len restriction in FindStart.m

--- FindStart.m
+++ FindStart.m
@@ -13,9 +13,9 @@ function [out outd] = FindStart(test_seq, ref_seq, end_list)
         LD = simmx(rev_test_seq', tmp1');
         [p q D] = dp(LD);
         [end_cand d]= FindEnd(D(end,:));
-        len = size(rev_test_seq,1);
-        end_cand = end_cand(end_cand>0.3*len&end_cand<2.5*len);
-        d = d(end_cand>0.3*len&end_cand<2.5*len);
+%         len = size(rev_test_seq,1);
+%         end_cand = end_cand(end_cand>0.3*len&end_cand<2.5*len);
+%         d = d(end_cand>0.3*len&end_cand<2.5*len);
         if (length(end_cand)==0)
             out(i) = -1;
             outd(i) = 9999999;
