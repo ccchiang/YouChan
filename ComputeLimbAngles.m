548790fecacb2d89faa24270f09bc11654e007a0
ccchiang <ccchiang.tw@gmail.com>
2013-07-05 20:17:54 +0800

    revise limb angle extraction

--- ComputeLimbAngles.m
+++ ComputeLimbAngles.m
@@ -1,55 +1,44 @@
 function out = ComputeLimbAngles(skeletons)
 no_skeletons = size(skeletons, 1);
-head = [3 4];
-shoulder = [5 9];
-torso = [2 3];
-hip = [13 17];
-ularm = [9 10];
-dlarm = [10 11];
-urarm = [5 6];
-drarm = [6 7];
-ulfoot = [17 18];
-dlfoot = [18 19];
-urfoot = [13 14];
-drfoot = [14 15];
-knees = [14 18];
-elbows = [6 10];
-wrists = [7 11];
-ankles = [15 19];
-lshoulder_wrist = [5 7];
-rshoulder_wrist = [9 11];
-lshoulder_sp = [1 5];
-rshoulder_sp = [1 9];
-lhip_elbow = [6 13];
-rhip_elbow = [10 17];
-all = [head;shoulder;torso;hip;ularm;dlarm;urarm;drarm;...
-    ulfoot;dlfoot;urfoot;drfoot;knees;elbows;wrists;ankles];
+head = [2 4];
+neck = [2 3];
+leftshoulder = [2 5];
+rightshoulder = [2 9];
+leftelbow = [2 6];
+rightelbow = [2 10];
+leftwrist = [2 7];
+rightwrist = [2 11];
+leftknee = [2 14];
+rightknee = [2 18];
+leftankel = [2 15];
+rightankel = [2 19];
+lefthand = [2 8];
+righthand = [2 12];
+leftfoot = [2 16];
+rightfoot = [2 20];
+all = [head;neck;leftshoulder;rightshoulder;leftelbow;...
+    rightelbow;leftwrist;rightwrist;leftknee;rightknee;...
+    leftankel;rightankel;lefthand;righthand;...
+    leftfoot;rightfoot];
 n = size(all, 1);
 vecs = zeros(no_skeletons, 3, n);
 tmpv = zeros(no_skeletons, 3, n);
-theta_xy = zeros(no_skeletons, n);
-theta_yz = zeros(no_skeletons, n);
-theta_xz = zeros(no_skeletons, n);
+theta_x = zeros(no_skeletons, n);
+theta_y = zeros(no_skeletons, n);
+theta_z = zeros(no_skeletons, n);
 v_xaxis = [ones(no_skeletons,1) zeros(no_skeletons,1) zeros(no_skeletons, 1)];
 v_yaxis = [zeros(no_skeletons,1) ones(no_skeletons,1) zeros(no_skeletons, 1)];
 v_zaxis = [zeros(no_skeletons,1) zeros(no_skeletons,1) ones(no_skeletons, 1)];
 for i=1:n
-    vecs(:,:,i) = skeletons(:,all(i,1)*3+1:all(i,1)*3+3)- ...
-                skeletons(:,all(i,2)*3+1:all(i,2)*3+3);
+    vecs(:,:,i) = skeletons(:,all(i,1)*3-2:all(i,1)*3)- ...
+                skeletons(:,all(i,2)*3-2:all(i,2)*3);
     tmpv(:,:,i) = vecs(:,:,i);
-    tmpv(:,3,i) = 0;
     vnorm = sqrt(tmpv(:,1,1).^2+tmpv(:,2,1).^2);
-    theta_xy(:,i) = sum(tmpv(:,:,i).*v_xaxis,2)./vnorm;
-    tmpv(:,:,i) = vecs(:,:,i);
-    tmpv(:,2,i) = 0;            
-    vnorm = sqrt(tmpv(:,1,1).^2+tmpv(:,2,1).^2);
-    theta_xz(:,i) = sum(tmpv(:,:,i).*v_zaxis,2)./vnorm;
-    tmpv(:,:,i) = vecs(:,:,i);
-    tmpv(:,1,i) = 0;            
-    vnorm = sqrt(tmpv(:,1,1).^2+tmpv(:,2,1).^2);
-    theta_yz(:,i) = sum(tmpv(:,:,i).*v_yaxis,2)./vnorm;
+    theta_x(:,i) = sum(tmpv(:,:,i).*v_xaxis,2)./vnorm;
+    theta_y(:,i) = sum(tmpv(:,:,i).*v_zaxis,2)./vnorm;
+    theta_z(:,i) = sum(tmpv(:,:,i).*v_yaxis,2)./vnorm;
 end
 out = zeros(no_skeletons, 3*n);
 for i = 1:n
-    out(:, 3*i-2:3*i) =  [theta_xy(:,i) theta_yz(:,i) theta_xz(:,i)];
+    out(:, 3*i-2:3*i) =  [theta_x(:,i) theta_y(:,i) theta_x(:,i)];
 end
